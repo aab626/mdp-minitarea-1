679916baa09cfc21e23170122ad1c67e707f9161
Augusto Aguayo <hostile.command@gmail.com>
2021-04-30 01:29:50 -0400

    Removed takeDamage method from Pokemon, now setHealthPoints does all the logic. Added JavaDoc for Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,10 +1,21 @@
+/**
+ * Representa un Pokémon mediante su nombre, especie, puntos de vida y tipo.
+ * Interactúa con otros Pokémon al atacarlos, donde el Pokémon atacado pierde puntos de vida.
+ * También puede quedar fuera de combate, cuando no le quedan puntos de vida.
+ */
 public class Pokemon {
     private String name;
     private String species;
     private int healthPoints;
     private IType type;
 
-    // Constructor
+    /**
+     * Constructor de Pokémon.
+     * @param name Nombre del Pokémon.
+     * @param species Especie del Pokémon.
+     * @param healthPoints Puntos de vida actuales del Pokémon.
+     * @param type Tipo del Pokémon.
+     */
     public Pokemon(String name, String species, int healthPoints, IType type){
         this.name = name;
         this.species = species;
@@ -12,60 +23,86 @@ public class Pokemon {
         this.type = type;
     }
 
-    // Getters
+    /**
+     * @return Nombre del Pokémon.
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * @return Especie del Pokémon.
+     */
     public String getSpecies(){
         return this.species;
     }
 
+    /**
+     * @return Puntos de vida actuales del Pokémon.
+     */
     public int getHealthPoints(){
         return this.healthPoints;
     }
 
+    /**
+     * @return Tipo del Pokémon.
+     */
     public IType getType(){
         return this.type;
     }
 
-    // Setters
+    /**
+     * Cambia el nombre del Pokémon.
+     * @param newName Nuevo nombre del Pokémon.
+     */
     public void setName(String newName){
         this.name = newName;
     }
 
+    /**
+     * Cambia la especie del Pokémon (En caso de ser necesario).
+     * @param newSpecies Nueva especie del Pokémon.
+     */
     public void setSpecies(String newSpecies){
         this.species = newSpecies;
     }
 
+    /**
+     * Cambia los Puntos de Vida actuales del Pokémon.
+     * Asegura que los puntos de vida no sean negativos.
+     * @param newHealthPoints Nueva cantidad de Puntos de Vida actuales del Pokémon.
+     */
     public void setHealthPoints(int newHealthPoints){
         this.healthPoints = newHealthPoints;
+        if (this.healthPoints < 0){
+            this.healthPoints = 0;
+        }
     }
 
-    // Representacion visual del Pokemon
+    /**
+     * @return Pokemon(<nombre>, <especie>, <puntos de vida>, <tipo>)
+     */
     @Override
     public String toString(){
-        String s = "Pokemon(name: " + this.name + ", species: " + this.species + ", hp: " + this.healthPoints + ", type: " + this.type + ")";
-        return s;
+        return "Pokemon(name: " + this.name + ", species: " + this.species + ", hp: " + this.healthPoints + ", type: " + this.type + ")";
     }
 
-    // Retorna true si esta fuera de combate, false si no
+    /**
+     * Responde a la pregunta: ¿El Pokémon esta fuera de combate?.
+     * @return true si el Pokémon esta fuera de combate (Puntos de vida igual a cero), false de lo contrario.
+     */
     public boolean isKnockedOut(){
         return this.healthPoints == 0;
     }
 
-    // ataque
+    /**
+     * Realiza un ataque desde el Pokémon a otro Pokémon.
+     * El daño del ataque está determinado por los tipos, y se sustrae ese daño de los puntos de vida del Pokémon atacado.
+     * @param otherPokemon El Pokémon que será atacado.
+     */
     public void attackPokemon(Pokemon otherPokemon){
         int damage = otherPokemon.type.getDamage(this.type);
-        otherPokemon.takeDamage(damage);
-    }
-
-    // Metodo para tomar daño, asegurar q el min sea 0
-    public void takeDamage(int damage){
-        this.healthPoints -= damage;
-        if (this.healthPoints < 0){
-            this.healthPoints = 0;
-        }
+        otherPokemon.setHealthPoints(otherPokemon.healthPoints - damage);
     }
 
 }
